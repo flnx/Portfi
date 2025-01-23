4e588c7daea8da61b674d1c5fb992dab746462bb
KartofaKurti <krafter420@gmail.com>
2025-01-23 19:14:37 +0200

    Models and DbContext Added

---
+++ core/Data/Enums/SocialMediaPlatforms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfiModels.Enums
+{
+    public enum SocialMediaPlatforms
+    {
+        GitHub = 1,
+        LinkedIn = 2,
+        Email = 3,
+        Instagram = 4,
+        X = 5,
+        YouTube = 6,
+        FaceBook = 7,
+        Pinterest = 8,
+        TikTok = 9,
+        Other = 10,
+
+    }
+}
